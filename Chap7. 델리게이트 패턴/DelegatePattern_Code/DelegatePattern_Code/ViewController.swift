5c11420cd68d207d402e6f5d80b230ca48c81eef
Taerogrammer <hyung54321@naver.com>
2023-09-17 20:28:43 +0900

    Chap7. 델리게이트 패턴 프로젝트 생성

---
+++ Chap7. 델리게이트 패턴/DelegatePattern_Code/DelegatePattern_Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegatePattern_Code
+//
+//  Created by 김태형 on 2023/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
